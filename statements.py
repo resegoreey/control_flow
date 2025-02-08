824ad0d9a0dbc8f9e8255018c8613a03d53735be
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-02-08 23:22:59 +0200

    wrote code to check which number is greater

--- statements.py
+++ statements.py
@@ -5,3 +5,17 @@ if number % 2 == 0:
     print(f"{number} is even")
 else:
     print(f"{number} is odd")
+print()
+
+#Compare two numbers and compare which one is larger
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+if num1 > num2:
+    print(f"{num1} is greater than {num2}")
+elif num2 > num1:
+    print(f"{num2} is greater than {num1}")
+else:
+    print("They are equal...")
+print()
+
